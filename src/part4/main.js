67dcba10e481599ea90910dde6e2963ecc89a9a6
JukkaKot <jukka.kotilainen@metropolia.fi>
2020-12-04 22:22:00 +0200

    koodin siivoomista

--- src/part4/main.js
+++ src/part4/main.js
@@ -20,17 +20,6 @@ for (var line of passArr) {
         p = []
     }
 }
-// byr (Birth Year) - four digits; at least 1920 and at most 2002.
-// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-// hgt (Height) - a number followed by either cm or in:
-// If cm, the number must be at least 150 and at most 193.
-// If in, the number must be at least 59 and at most 76.
-// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-// pid (Passport ID) - a nine-digit number, including leading zeroes.
-
-// cid (Country ID) - ignored, missing or not.
 var p1Valids = []
 for (p of passports) {
     if (p.length === 7) {
@@ -45,22 +34,13 @@ for (p of p1Valids) {
         switch (field[0]) {
             case "byr":
                 // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-                if (!(Number(value) >= 1920 && Number(value) <= 2002)) {
-                    isValid = false
-                }
-                break;
+                if (!(Number(value) >= 1920 && Number(value) <= 2002)) isValid = false; break;
             case "iyr":
                 // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-                if (!(Number(value) >= 2010 && Number(value) <= 2020)) {
-                    isValid = false
-                }
-                break;
+                if (!(Number(value) >= 2010 && Number(value) <= 2020)) isValid = false; break;
             case "eyr":
                 // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-                if (!(Number(value) >= 2020 && Number(value) <= 2030)) {
-                    isValid = false
-                }
-                break;
+                if (!(Number(value) >= 2020 && Number(value) <= 2030)) isValid = false; break;
             case "hgt":
                 // hgt (Height) - a number followed by either cm or in:
                 // If cm, the number must be at least 150 and at most 193.
@@ -70,63 +50,34 @@ for (p of p1Valids) {
                 const number = splitValue[0]
                 if (unit) {
                     if (unit === "cm") {
-                        if (!(Number(number) >= 150 && Number(number) <= 193)){
-                            isValid = false
-                            break;
-                        }
+                        if (!(Number(number) >= 150 && Number(number) <= 193)) isValid = false; break;
                     } else if (unit === "in") {
-                        if (!(Number(number) >= 59 && Number(number) <= 76)){
-                            isValid = false
-                            break;
-                        }
-                    } else {
-                        isValid = false
-                        break;
-                    }
-                } else {
-                    isValid = false
-                    break;
-                }
+                        if (!(Number(number) >= 59 && Number(number) <= 76)) isValid = false; break;
+                    } else isValid = false; break;
+                } else isValid = false;
                 break;
             case "hcl":
                 // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-                if(!value.startsWith("#")){
-                    isValid=false
-                    break;
-                }
-                if(value.length !== 7){
-                    isValid=false
-                }
+                if (!value.startsWith("#") || value.length !== 7) isValid = false
                 break;
             case "ecl":
                 // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-                const eyeColors = ["amb","blu","brn","gry","grn","hzl","oth"]
-                if(!eyeColors.includes(value)){
-                    console.log("ei valid",field)
-                    isValid = false
-                }
-                
+                const eyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+                if (!eyeColors.includes(value)) isValid = false
                 break;
             case "pid":
                 // pid (Passport ID) - a nine-digit number, including leading zeroes.
-                if(!value.match(/^\d{9}$/)){
-                    isValid = false
-                    
-                    break;
-                }
-                
+                if (!value.match(/^\d{9}$/))  isValid = false
                 break;
             default:
-            //  console.log("Ei pitäs mennä mutta okei?")
         }
         if (!isValid) {
             break
         }
     }
     if (isValid) {
-        // console.log("validi passport:",p)
         p2Valids.push(p)
     }
 }
-console.log("part1 Valid count:", p1Valids.length)
-console.log("part2 Valid count:", p2Valids.length)
+console.log("part1 Valids:", p1Valids.length)
+console.log("part2 Valids:", p2Valids.length)
